82a7fb3293afb207c1891d7b8e17e46d646a9061
Allen Robel <arobel@cisco.com>
2023-01-11 10:08:33 -1000

    run thru black

--- lib/ndfc_python/ndfc_fabric_site.py
+++ lib/ndfc_python/ndfc_fabric_site.py
@@ -1,178 +1,238 @@
-our_version = 100
-import json
+"""
+Name: ndfc_fabric_site.py
+Description: NdfcFabricSite creates NDFC site fabrics
+"""
+import sys
+
+OUR_VERSION = 101
+
+
 class NdfcFabricSite(object):
-    '''
+    """
     Create site/child fabrics.
 
     Example create operation:
 
     instance = NdfcFabricSite(ndfc)
     instance.fabric = 'bang'
-    instance.BGP_AS = 65011
-    instance.REPLICATION_MODE = 'Ingress'
+    instance.bgp_as = 65011
+    instance.replication_mode = 'Ingress'
     instance.create()
 
     TODO: Need a delete() method
-    '''
+    """
+
     def __init__(self, ndfc):
-        self.lib_version = our_version
+        self.lib_version = OUR_VERSION
         self.class_name = __class__.__name__
         self.ndfc = ndfc
 
+        self._init_properties_set()
+        self._init_properties_mandatory_set()
+        self._init_nv_pairs_default()
+        self._init_nv_pairs_set()
+        self._init_nv_pairs_mandatory_set()
+        self._init_properties_default()
+        self._init_properties()
+        self._init_nv_pairs()
+
+    def _init_properties_set(self):
         self.properties_set = set()
-        self.properties_set.add('fabric')
+        self.properties_set.add("fabric")
 
+    def _init_properties_mandatory_set(self):
         self.properties_mandatory_set = set()
-        self.properties_mandatory_set.add('fabric')
+        self.properties_mandatory_set.add("fabric")
 
-        self.properties_default = dict()
+    def _init_nv_pairs_default(self):
+        self._nv_pairs_default = {}
+        self._nv_pairs_default["ANYCAST_RP_IP_RANGE"] = ""
+        self._nv_pairs_default["DCI_SUBNET_RANGE"] = "10.33.0.0/16"
+        self._nv_pairs_default["FABRIC_MTU"] = "9216"
+        self._nv_pairs_default["LOOPBACK0_IP_RANGE"] = "10.2.0.0/22"
+        self._nv_pairs_default["LOOPBACK1_IP_RANGE"] = "10.3.0.0/22"
+        self._nv_pairs_default["REPLICATION_MODE"] = "Ingress"
+        self._nv_pairs_default["SUBNET_RANGE"] = "10.4.0.0/16"
+        self._nv_pairs_default[
+            "network_extension_template"
+        ] = "Default_Network_Extension_Universal"
+        self._nv_pairs_default[
+            "vrf_extension_template"
+        ] = "Default_VRF_Extension_Universal"
 
+    def _init_nv_pairs_set(self):
+        self._nv_pairs_set = set()
+        self._nv_pairs_set.add("ANYCAST_RP_IP_RANGE")
+        self._nv_pairs_set.add("BGP_AS")
+        self._nv_pairs_set.add("DCI_SUBNET_RANGE")
+        self._nv_pairs_set.add("FABRIC_MTU")
+        self._nv_pairs_set.add("LOOPBACK0_IP_RANGE")
+        self._nv_pairs_set.add("LOOPBACK1_IP_RANGE")
+        self._nv_pairs_set.add("REPLICATION_MODE")
+        self._nv_pairs_set.add("SUBNET_RANGE")
+        self._nv_pairs_set.add("network_extension_template")
+        self._nv_pairs_set.add("vrf_extension_template")
 
-        self.nv_pairs_set = set()
-        self.nv_pairs_set.add('ANYCAST_RP_IP_RANGE')
-        self.nv_pairs_set.add('BGP_AS')
-        self.nv_pairs_set.add('DCI_SUBNET_RANGE')
-        self.nv_pairs_set.add('FABRIC_MTU')
-        self.nv_pairs_set.add('LOOPBACK0_IP_RANGE')
-        self.nv_pairs_set.add('LOOPBACK1_IP_RANGE')
-        self.nv_pairs_set.add('REPLICATION_MODE')
-        self.nv_pairs_set.add('SUBNET_RANGE')
-        self.nv_pairs_set.add('network_extension_template')
-        self.nv_pairs_set.add('vrf_extension_template')
-
-        self.nv_pairs_default = dict()
-        self.nv_pairs_default['ANYCAST_RP_IP_RANGE'] = ''
-        self.nv_pairs_default['DCI_SUBNET_RANGE'] = '10.33.0.0/16'
-        self.nv_pairs_default['FABRIC_MTU'] = '9216'
-        self.nv_pairs_default['LOOPBACK0_IP_RANGE'] = '10.2.0.0/22'
-        self.nv_pairs_default['LOOPBACK1_IP_RANGE'] = '10.3.0.0/22'
-        self.nv_pairs_default['REPLICATION_MODE'] = 'Ingress'
-        self.nv_pairs_default['SUBNET_RANGE'] = '10.4.0.0/16'
-        self.nv_pairs_default['network_extension_template'] = 'Default_Network_Extension_Universal'
-        self.nv_pairs_default['vrf_extension_template'] = 'Default_VRF_Extension_Universal'
-
+    def _init_nv_pairs_mandatory_set(self):
         self.nv_pairs_mandatory_set = set()
-        self.nv_pairs_mandatory_set = self.nv_pairs_set.difference(
-            self.nv_pairs_default
+        self.nv_pairs_mandatory_set = self._nv_pairs_set.difference(
+            self._nv_pairs_default
         )
 
+    def _init_properties_default(self):
+        self.properties_default = {}
+
+    def _init_properties(self):
+        self.properties = {}
+        for param in self.properties_set:
+            if param in self.properties_default:
+                self.properties[param] = self.properties_default[param]
+            else:
+                self.properties[param] = ""
+
+    def _init_nv_pairs(self):
         # All properties, except fabric, live here
-        self.nvPairs = dict()
-
-        self.init_properties()
-        self.init_nvPairs()
-
-    def init_properties(self):
-        self.properties = dict()
-        for p in self.properties_set:
-            if p in self.properties_default:
-                self.properties[p] = self.properties_default[p]
+        self._nv_pairs = {}
+        for param in self._nv_pairs_set:
+            if param in self._nv_pairs_default:
+                self._nv_pairs[param] = self._nv_pairs_default[param]
             else:
-                self.properties[p] = ""
+                self._nv_pairs[param] = ""
 
-    def init_nvPairs(self):
-        for p in self.nv_pairs_set:
-            if p in self.nv_pairs_default:
-                self.nvPairs[p] = self.nv_pairs_default[p]
-            else:
-                self.nvPairs[p] = ""
-
-    def preprocess_properties(self):
-        '''
-        1. Align the properties to the expectations of NDFC 
-        '''
+    def _preprocess_properties(self):
+        """
+        1. Align the properties to the expectations of NDFC
+        """
         pass
 
-    def final_verification(self):
-        for p in self.properties_mandatory_set:
-            if self.properties[p] == "":
-                self.ndfc.log.error('exiting. call instance.{} before calling instance.post()'.format(p))
-                exit(1)
-        for p in self.nv_pairs_mandatory_set:
-            if self.nvPairs[p] == "":
-                self.ndfc.log.error('exiting. call instance.{} before calling instance.post()'.format(p))
-                exit(1)
+    def _final_verification(self):
+        for param in self.properties_mandatory_set:
+            if self.properties[param] == "":
+                msg = f"exiting. call instance.{param.lower()} before calling instance.post()"
+                self.ndfc.log.error(msg)
+                sys.exit(1)
+        for param in self.nv_pairs_mandatory_set:
+            if self._nv_pairs[param] == "":
+                msg = f"exiting. call instance.{param.lower()} before calling instance.post()"
+                self.ndfc.log.error(msg)
+                sys.exit(1)
 
     def create(self):
-        self.final_verification()
-        self.preprocess_properties()
+        self._final_verification()
+        self._preprocess_properties()
 
-        url = 'https://{}/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/{}/Easy_Fabric'.format(self.ndfc.ip, self.fabric)
+        url = f"{self.ndfc.url_control_fabrics}/{self.fabric}/Easy_Fabric"
 
-        headers = dict()
-        headers['Authorization'] = self.ndfc.bearer_token
-        headers['Content-Type'] = 'application/json'
+        headers = {}
+        headers["Authorization"] = self.ndfc.bearer_token
+        headers["Content-Type"] = "application/json"
 
-        self.ndfc.post(url, headers, self.nvPairs)
+        self.ndfc.post(url, headers, self._nv_pairs)
 
     # top_level properties
     @property
     def fabric(self):
-        return self.properties['fabric']
+        """
+        Return user-specified fabric name
+        """
+        return self.properties["fabric"]
+
     @fabric.setter
-    def fabric(self, x):
-        self.properties['fabric'] = x
+    def fabric(self, param):
+        self.properties["fabric"] = param
 
     # nvPairs
     @property
-    def ANYCAST_RP_IP_RANGE(self):
-        return self.nvPairs['ANYCAST_RP_IP_RANGE']
-    @ANYCAST_RP_IP_RANGE.setter
-    def ANYCAST_RP_IP_RANGE(self, x):
-        self.ndfc.verify_ipv4_address_with_prefix(x)
-        self.nvPairs['ANYCAST_RP_IP_RANGE'] = x
+    def anycast_rp_ip_range(self):
+        """
+        return the current nv_pairs value of anycast_rp_ip_range
+        """
+        return self._nv_pairs["ANYCAST_RP_IP_RANGE"]
+
+    @anycast_rp_ip_range.setter
+    def anycast_rp_ip_range(self, param):
+        self.ndfc.verify_ipv4_address_with_prefix(param)
+        self._nv_pairs["ANYCAST_RP_IP_RANGE"] = param
 
     @property
-    def BGP_AS(self):
-        return self.nvPairs['BGP_AS']
-    @BGP_AS.setter
-    def BGP_AS(self, x):
-        self.ndfc.verify_bgp_asn(x)
-        self.nvPairs['BGP_AS'] = x
+    def bgp_as(self):
+        """
+        return the current nv_pairs value of bgp_as
+        """
+        return self._nv_pairs["BGP_AS"]
+
+    @bgp_as.setter
+    def bgp_as(self, param):
+        self.ndfc.verify_bgp_asn(param)
+        self._nv_pairs["BGP_AS"] = param
 
     @property
-    def DCI_SUBNET_RANGE(self):
-        return self.nvPairs['DCI_SUBNET_RANGE']
-    @DCI_SUBNET_RANGE.setter
-    def DCI_SUBNET_RANGE(self, x):
-        self.ndfc.verify_ipv4_address_with_prefix(x)
-        self.nvPairs['DCI_SUBNET_RANGE'] = x
+    def dci_subnet_range(self):
+        """
+        return the current nv_pairs value of dci_subnet_range
+        """
+        return self._nv_pairs["DCI_SUBNET_RANGE"]
+
+    @dci_subnet_range.setter
+    def dci_subnet_range(self, param):
+        self.ndfc.verify_ipv4_address_with_prefix(param)
+        self._nv_pairs["DCI_SUBNET_RANGE"] = param
 
     @property
-    def FABRIC_MTU(self):
-        return self.nvPairs['FABRIC_MTU']
-    @FABRIC_MTU.setter
-    def FABRIC_MTU(self, x):
-        self.nvPairs['FABRIC_MTU'] = x
+    def fabric_mtu(self):
+        """
+        return the current nv_pairs value of fabric_mtu
+        """
+        return self._nv_pairs["FABRIC_MTU"]
+
+    @fabric_mtu.setter
+    def fabric_mtu(self, param):
+        self._nv_pairs["FABRIC_MTU"] = param
 
     @property
-    def LOOPBACK0_IP_RANGE(self):
-        return self.nvPairs['LOOPBACK0_IP_RANGE']
-    @LOOPBACK0_IP_RANGE.setter
-    def LOOPBACK0_IP_RANGE(self, x):
-        self.ndfc.verify_ipv4_address_with_prefix(x)
-        self.nvPairs['LOOPBACK0_IP_RANGE'] = x
+    def loopback0_ip_range(self):
+        """
+        return the current nv_pairs value of loopback0_ip_range
+        """
+        return self._nv_pairs["LOOPBACK0_IP_RANGE"]
+
+    @loopback0_ip_range.setter
+    def loopback0_ip_range(self, param):
+        self.ndfc.verify_ipv4_address_with_prefix(param)
+        self._nv_pairs["LOOPBACK0_IP_RANGE"] = param
 
     @property
-    def LOOPBACK1_IP_RANGE(self):
-        return self.nvPairs['LOOPBACK1_IP_RANGE']
-    @LOOPBACK1_IP_RANGE.setter
-    def LOOPBACK1_IP_RANGE(self, x):
-        self.ndfc.verify_ipv4_address_with_prefix(x)
-        self.nvPairs['LOOPBACK1_IP_RANGE'] = x
+    def loopback1_ip_range(self):
+        """
+        return the current nv_pairs value of loopback1_ip_range
+        """
+        return self._nv_pairs["LOOPBACK1_IP_RANGE"]
+
+    @loopback1_ip_range.setter
+    def loopback1_ip_range(self, param):
+        self.ndfc.verify_ipv4_address_with_prefix(param)
+        self._nv_pairs["LOOPBACK1_IP_RANGE"] = param
 
     @property
-    def REPLICATION_MODE(self):
-        return self.nvPairs['REPLICATION_MODE']
-    @REPLICATION_MODE.setter
-    def REPLICATION_MODE(self, x):
-        self.ndfc.verify_replication_mode(x)
-        self.nvPairs['REPLICATION_MODE'] = x
+    def replication_mode(self):
+        """
+        return the current nv_pairs value of replication_mode
+        """
+        return self._nv_pairs["REPLICATION_MODE"]
+
+    @replication_mode.setter
+    def replication_mode(self, param):
+        self.ndfc.verify_replication_mode(param)
+        self._nv_pairs["REPLICATION_MODE"] = param
 
     @property
-    def SUBNET_RANGE(self):
-        return self.nvPairs['SUBNET_RANGE']
-    @SUBNET_RANGE.setter
-    def SUBNET_RANGE(self, x):
-        self.ndfc.verify_ipv4_address_with_prefix(x)
-        self.nvPairs['SUBNET_RANGE'] = x
+    def subnet_range(self):
+        """
+        return the current nv_pairs value of subnet_range
+        """
+        return self._nv_pairs["SUBNET_RANGE"]
+
+    @subnet_range.setter
+    def subnet_range(self, param):
+        self.ndfc.verify_ipv4_address_with_prefix(param)
+        self._nv_pairs["SUBNET_RANGE"] = param
