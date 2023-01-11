e893737643ab93d8730554c36c25663921731845
Allen Robel <arobel@cisco.com>
2023-01-11 13:50:47 -1000

    run thru black/pylint

--- examples/example_ndfc_load_config.py
+++ examples/example_ndfc_load_config.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
-# Edit the config_file variable at the top of ndfc-python/ndfc_lib/ndfc_config.py 
-# to point to your Ansible vault file
+"""
+Name: example_ndfc_load_config.py
+Description:
+
+Load the configuration file pointed to in lib/ndfc_python/ndfc_config.py
+and print the parameters and values contained therein.
+
+Usage:
+
+Edit the config_file variable at the top of ndfc-python/lib/ndfc_python/ndfc_config.py
+to point to your Ansible vault file
+"""
 
 from ndfc_python.ndfc_config import NdfcLoadConfig
 
 c = NdfcLoadConfig()
-print('c.ansible_vault {}'.format(c.config['ansible_vault']))
+print(f"c.ansible_vault {c.config['ansible_vault']}")
