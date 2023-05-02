2377aef3062f962e5369867267d2958b58e8c17c
Allen Robel <arobel@cisco.com>
2023-05-02 12:18:18 -1000

    Make the linters happy

--- lib/ndfc_python/ndfc_discover.py
+++ lib/ndfc_python/ndfc_discover.py
@@ -1,11 +1,7 @@
-import sys
-import json
-from time import sleep
-OUR_VERSION = 102
-'''
-Discover switch.
+"""
+Initiate NDFC discovery of switches.
 REST: POST
-URL: https://10.195.225.167/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/2/inventory/discover
+URL: https://<ip>/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics/2/inventory/discover
 PAYLOAD:  The JSON payload constructed by the NdfcDiscover() class is shown below.
 
     { "maxHops":"0",
@@ -25,12 +21,25 @@ PAYLOAD:  The JSON payload constructed by the NdfcDiscover() class is shown belo
     "username":"admin",
     "password":"myPassword",
     "preserveConfig":false,
-    "switches":[{"ipaddr":"10.1.150.104","sysName":"cvd-1313-leaf","deviceIndex":"cvd-1313-leaf(FDO211218HH)","platform":"N9K-C93180YC-EX","version":"10.2(3)","serialNumber":"FDO211218HH","vdcId":0,"vdcMac":null}]}
+    "switches":[
+        {
+            "ipaddr":"10.1.150.104",
+            "sysName":"cvd-1313-leaf",
+            "deviceIndex":"cvd-1313-leaf(FDO011218HH)",
+            "platform":"N9K-C93180YC-EX",
+            "version":"10.2(3)",
+            "serialNumber":"FDO011218HH",
+            "vdcId":0,
+            "vdcMac":null
+        }
+    ]
+}
 
 
 
 
-Response from NdfcDiscover() class is shown below.  Note, this can be used as the "switches" value in the NdfcDiscover() payload:
+Response from NdfcDiscover() class is shown below.
+Note, this can be used as the "switches" value in the NdfcDiscover() payload:
 
     [
         {
@@ -40,7 +49,7 @@ Response from NdfcDiscover() class is shown below.  Note, this can be used as th
             "valid":true,
             "selectable":true,
             "sysName":"cvd-1314-leaf",
-            "serialNumber":"FDO211218FV",
+            "serialNumber":"FDO011218FV",
             "vdcMac":null,
             "vdcId":0,
             "ipaddr":"10.1.150.105",
@@ -48,13 +57,20 @@ Response from NdfcDiscover() class is shown below.  Note, this can be used as th
             "version":"10.2(3)",
             "lastChange":null,
             "hopCount":0,
-            "deviceIndex":"cvd-1314-leaf(FDO211218FV)",
+            "deviceIndex":"cvd-1314-leaf(FDO011218FV)",
             "statusReason":"manageable"
         }
     ]
-'''
-class NdfcDiscover(object):
-    '''
+"""
+import json
+import sys
+from time import sleep
+
+OUR_VERSION = 103
+
+
+class NdfcDiscover:
+    """
     Discover switches
     Populates:
         self.result = result code from controller
@@ -70,7 +86,8 @@ class NdfcDiscover(object):
     instance.discover()
     print('response: {}'.format(instance.response))
 
-    '''
+    """
+
     def __init__(self, ndfc):
         self.lib_version = OUR_VERSION
         self.class_name = __class__.__name__
@@ -80,41 +97,44 @@ class NdfcDiscover(object):
         self._retry_sleep_time = 10
 
         # post/get base headers
-        self.headers = dict()
-        self.headers['Content-Type'] = 'application/json'
+        self.headers = {}
+        self.headers["Content-Type"] = "application/json"
 
         self.payload_set = set()
-        self.payload_set.add('cdpSecondTimeout')
-        self.payload_set.add('fabric')
-        self.payload_set.add('maxHops')
-        self.payload_set.add('password')
-        self.payload_set.add('preserveConfig')
-        self.payload_set.add('seedIP')
-        self.payload_set.add('snmpV3AuthProtocol')
-        self.payload_set.add('username')
+        self.payload_set.add("cdpSecondTimeout")
+        self.payload_set.add("fabric")
+        self.payload_set.add("maxHops")
+        self.payload_set.add("password")
+        self.payload_set.add("preserveConfig")
+        self.payload_set.add("seedIP")
+        self.payload_set.add("snmpV3AuthProtocol")
+        self.payload_set.add("username")
 
         self.payload_set_mandatory = set()
-        self.payload_set_mandatory.add('fabric')
-        self.payload_set_mandatory.add('password')
-        self.payload_set_mandatory.add('seedIP')
+        self.payload_set_mandatory.add("fabric")
+        self.payload_set_mandatory.add("password")
+        self.payload_set_mandatory.add("seedIP")
 
-        self.payload_default = dict()
-        self.payload_default['cdpSecondTimeout'] = 5
-        self.payload_default['maxHops'] = 0
-        self.payload_default['preserveConfig'] = True
-        self.payload_default['snmpV3AuthProtocol'] = 0
-        self.payload_default['username'] = 'admin'
+        self.payload_default = {}
+        self.payload_default["cdpSecondTimeout"] = 5
+        self.payload_default["maxHops"] = 0
+        self.payload_default["preserveConfig"] = True
+        self.payload_default["snmpV3AuthProtocol"] = 0
+        self.payload_default["username"] = "admin"
 
         self.response = ""
         self.discover_status_code = -1
         self.reachability_status_code = -1
-        self.reachability_response = dict()
-        self.discover_response = dict()
+        self.reachability_response = {}
+        self.discover_response = {}
 
         self.init_payload()
 
     def init_payload(self):
-        self.payload = dict()
+        """
+        Initialize the payload for the request body.
+        """
+        self.payload = {}
         for p in self.payload_set:
             if p in self.payload_default:
                 self.payload[p] = self.payload_default[p]
@@ -122,16 +142,19 @@ class NdfcDiscover(object):
                 self.payload[p] = ""
 
     def preprocess_payload(self):
-        '''
-        1. Set a default value for any properties that the caller has not set and that NDFC provides a default for. 
-
-        2. Copy top-level property values (that need it) into their respective template_config properties.
+        """
+        1. Set a default value for any properties that the caller has not set and
+        that NDFC provides a default for.
 
         3. Any other fixup that may be required
-        '''
-        pass
+        """
 
     def final_verification(self):
+        """
+        Set of final checks prior to sending the request.
+
+        1. Verify all mandatory parameters have been set
+        """
         for key in self.payload_set_mandatory:
             if self.payload[key] == "":
                 self.ndfc.log.error(
@@ -140,6 +163,10 @@ class NdfcDiscover(object):
                 sys.exit(1)
 
     def is_reachable(self):
+        """
+        Return True if self.seedIP is reachable, per test-reachability endpoint.
+        Return False otherwise.
+        """
         url = f"{self.ndfc.url_control_fabrics}/{self.fabric_name}/inventory/test-reachability"
         self.ndfc.post(url, self.ndfc.make_headers(), self.payload)
         self.reachability_status_code = self.ndfc.response.status_code
@@ -154,6 +181,9 @@ class NdfcDiscover(object):
         return True
 
     def discover(self):
+        """
+        Issue POST to NDFC's discover endpoint with validated request.
+        """
         self.preprocess_payload()
         self.final_verification()
         retries = 4
@@ -166,92 +196,135 @@ class NdfcDiscover(object):
 
         url = f"{self.ndfc.url_control_fabrics}/{self.fabric_name}/inventory/discover"
 
-        self.payload['switches'] = self.reachability_response
-        self.ndfc.log.info('self.payload {}'.format(self.payload))
+        self.payload["switches"] = self.reachability_response
+        self.ndfc.log.info(f"self.payload {self.payload}")
 
         self.ndfc.post(url, self.ndfc.make_headers(), self.payload)
         self.discover_status_code = self.ndfc.response.status_code
         self.discover_response = json.loads(self.ndfc.response.text)
 
     def is_up(self):
+        """
+        Return True if self.seedIP is manageable.
+        Return False otherwise.
+        """
         url = f"{self.ndfc.url_control_fabrics}/{self.fabric_name}/inventory/switchesByFabric"
         headers = self.ndfc.make_headers()
         self.ndfc.get(url, headers)
         response = json.loads(self.ndfc.response.text)
-        found = None
+        our_switch = None
         for item in response:
             if "ipAddress" not in item:
                 continue
             # The key 'manageable' is mispelled in the response
             if "managable" not in item:
-                self.ndfc.log.error(f"skipping due to 'managable [sic]' not in response {item}")
+                message = (
+                    f"Skipping. 'managable' [sic] key not found in response {item}"
+                )
+                self.ndfc.log.error(message)
                 continue
             if item["ipAddress"] == self.seedIP:
-                found = item
+                our_switch = item
                 break
-        if found is None:
+        if our_switch is None:
             self.ndfc.log.warning(f"{self.seedIP} not found. Returning False")
             return False
-        return item["managable"]
+        return our_switch["managable"]
 
     # top_level properties
     @property
     def cdpSecondTimeout(self):
-        return self.payload['cdpSecondTimeout']
+        """
+        The number of seconds allowed for CDP discovery of neighbor switches
+        """
+        return self.payload["cdpSecondTimeout"]
+
     @cdpSecondTimeout.setter
     def cdpSecondTimeout(self, x):
-        self.ndfc.verify_digits(x, 'cdpSecondTimeout')
-        self.payload['cdpSecondTimeout'] = x
+        self.ndfc.verify_digits(x, "cdpSecondTimeout")
+        self.payload["cdpSecondTimeout"] = x
 
     @property
     def fabric_name(self):
-        return self.payload['fabric']
+        """
+        The name of the fabric in which discovery will take place
+        """
+        return self.payload["fabric"]
+
     @fabric_name.setter
     def fabric_name(self, x):
-        self.payload['fabric'] = x
+        self.payload["fabric"] = x
 
     @property
     def maxHops(self):
-        return self.payload['maxHops']
+        """
+        The radius, in hops, from the seedIP switch, in which to
+        discover neighboring switches.  If set to 0, discovery is
+        limited to the seedIP switch.  If set to > 0, discovery
+        will include switches X hops away from the seedIP switch.
+        """
+        return self.payload["maxHops"]
+
     @maxHops.setter
     def maxHops(self, x):
-        self.ndfc.verify_digits(x, 'maxHops')
-        self.payload['maxHops'] = x
+        self.ndfc.verify_digits(x, "maxHops")
+        self.payload["maxHops"] = x
 
     @property
     def password(self):
-        return self.payload['password']
+        """
+        The password of the seedIP switch and any discovered switches.
+        """
+        return self.payload["password"]
+
     @password.setter
     def password(self, x):
-        self.payload['password'] = x
+        self.payload["password"] = x
 
     @property
     def preserveConfig(self):
-        return self.template_config['preserveConfig']
+        """
+        If set to True, the configurations on the discovered switches will be preserved.
+        Else, the configurations on the discovered switches will be erased.
+        """
+        return self.payload["preserveConfig"]
+
     @preserveConfig.setter
     def preserveConfig(self, x):
         self.ndfc.verify_boolean(x)
-        self.payload['preserveConfig'] = x
+        self.payload["preserveConfig"] = x
 
     @property
     def seedIP(self):
-        return self.payload['seedIP']
+        """
+        The IP address of the switch from which discovery will be initiated.
+        """
+        return self.payload["seedIP"]
+
     @seedIP.setter
     def seedIP(self, x):
-        self.ndfc.verify_ipv4_address(x, 'seedIP')
-        self.payload['seedIP'] = x
+        self.ndfc.verify_ipv4_address(x, "seedIP")
+        self.payload["seedIP"] = x
 
     @property
     def snmpV3AuthProtocol(self):
-        return self.payload['snmpV3AuthProtocol']
+        """
+        The SNMPv3 authorization protocol to configure on discovered switches.
+        """
+        return self.payload["snmpV3AuthProtocol"]
+
     @snmpV3AuthProtocol.setter
     def snmpV3AuthProtocol(self, x):
-        self.ndfc.verify_digits(x, 'snmpV3AuthProtocol')
-        self.payload['snmpV3AuthProtocol'] = x
+        self.ndfc.verify_digits(x, "snmpV3AuthProtocol")
+        self.payload["snmpV3AuthProtocol"] = x
 
     @property
     def username(self):
-        return self.payload['username']
+        """
+        The username of the seedIP switch and any discovered switches.
+        """
+        return self.payload["username"]
+
     @username.setter
     def username(self, x):
-        self.payload['username'] = x
+        self.payload["username"] = x
