2d257aca0078a8c262933d01c97b91567e179726
Allen Robel <arobel@cisco.com>
2023-07-22 15:33:41 -1000

    appease linters

--- lib/ndfc_python/ndfc_config.py
+++ lib/ndfc_python/ndfc_config.py
@@ -3,8 +3,10 @@ ndfc_config.py
 
 Description:
 
-Load YAML file pointed to by the config_file variable, verify all mandatory keys are present,
-and return the contents as a python dict()
+Load YAML file pointed to by CONFIG_FILE, verify all mandatory keys are
+present, and return the contents as a python dict()
+
+Edit the CONFIG_FILE constant below to point to your configuration file.
 
 Usage:
 
@@ -13,24 +15,27 @@ c = NdfcLoadConfig()
 print(f"c.ansible_vault {c.config['ansible_vault']}")
 """
 import sys
+from os import environ
 
 import yaml
 
-config_file = "/Users/arobel/repos/ndfc-python/lib/ndfc_python/config/config.yml"
+REPOS = f"{environ['HOME']}/repos"
+# Edit me!
+CONFIG_FILE = f"{REPOS}/ndfc-python/lib/ndfc_python/config/config.yml"
 
 
 class NdfcLoadConfig:
     """
-    When instantiated, load the YAML file pointed to by config_file (at the
+    When instantiated, load the YAML file pointed to by CONFIG_FILE (at the
     top of this file) and set self.properties["config"] to the contents of
-    this file.  self.properties["config"] will be a python dictionary representing
-    the contents of config_file.
+    this file.  self.properties["config"] will be a python dictionary
+    representing the contents of CONFIG_FILE.
 
-    The (current) mandatory keys in config_file are:
+    The (current) mandatory keys in CONFIG_FILE are:
 
     ansible_vault : path to the Ansible Vault file to load.
 
-    Example config_file:
+    Example CONFIG_FILE:
 
     ansible_vault: "/path/to/my/ansible/vault"
 
@@ -51,39 +56,38 @@ class NdfcLoadConfig:
         for key in self.mandatory_keys:
             if key in self.properties["config"]:
                 continue
-            print(
-                f"Exiting. {__class__.__name__}.load_config() - missing mandatory key '{key}'"
-                f" in config_file {config_file}"
-            )
+            msg = f"Exiting. {__class__.__name__}.load_config() - missing "
+            msg += f"mandatory key '{key}' in CONFIG_FILE {CONFIG_FILE}"
+            print(msg)
             sys.exit(1)
 
     def load_config(self):
         """
-        Open the YAML file, config_file, and load its contents
+        Open the YAML file, CONFIG_FILE, and load its contents
         into self.properties["config"]
         """
         try:
-            with open(config_file, "r", encoding="utf-8") as fp:
-                self.properties["config"] = yaml.safe_load(fp)
+            with open(CONFIG_FILE, "r", encoding="utf-8") as handle:
+                self.properties["config"] = yaml.safe_load(handle)
         except FileNotFoundError as exception:
             print(
                 f"Exiting. {__class__.__name__}."
-                f"load_config() - config_file not found: {config_file}"
+                f"load_config() - CONFIG_FILE not found: {CONFIG_FILE}"
                 f" Exception detail: {exception}"
             )
             sys.exit(1)
         except EOFError as exception:
             print(
                 f"Exiting. {__class__.__name__}."
-                f"load_config() - config_file {config_file} has no contents?"
+                f"load_config() - CONFIG_FILE {CONFIG_FILE} has no contents?"
                 f" Exception detail: {exception}"
             )
             sys.exit(1)
         if self.properties["config"] is None:
-            print(
-                f"Exiting. {__class__.__name__}."
-                f"load_config() - No key/values found in config_file {config_file}"
-            )
+            msg = f"Exiting. {__class__.__name__}. "
+            msg += "load_config() - No key/values found in CONFIG_FILE "
+            msg += f"{CONFIG_FILE}"
+            print(msg)
             sys.exit(1)
 
     @property
