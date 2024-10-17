84599143ef004f63a143fe61f64373bf9bd529c0
Allen Robel <arobel@cisco.com>
2024-10-16 16:02:44 -1000

    Update example scripts with new logging framework
    - examples/ndfc_raw_get_request.py
    - examples/ndfc_raw_put_request.py

--- examples/ndfc_raw_get_request.py
+++ examples/ndfc_raw_get_request.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python3
 """
-Example of a raw GET request to NDFC
+Example: send a raw GET request to NDFC
 """
 import json
 
-from ndfc_python.log import log
+from ndfc_python.log_v2 import Log
 from ndfc_python.ndfc import NDFC
 from ndfc_python.ndfc_credentials import NdfcCredentials
 
-logger = log("ndfc_login_log", "INFO", "DEBUG")
+try:
+    log = Log()
+    log.commit()
+except ValueError as error:
+    MSG = "Error while instantiating Log(). "
+    MSG += f"Error detail: {error}"
+    print(MSG)
+
 nc = NdfcCredentials()
 ndfc = NDFC()
 ndfc.domain = nc.nd_domain
 ndfc.ip4 = nc.ndfc_ip
-ndfc.logger = logger
 ndfc.password = nc.password
 ndfc.username = nc.username
 ndfc.login()
-url_issu = f"https://{ndfc.ip4}"
+url_base = f"https://{ndfc.ip4}"
+
+# Some example URLs
+url_issu = f"{url_base}"
 url_issu += "/appcenter/cisco/ndfc/api/v1/imagemanagement/rest/packagemgnt/issu"
-url = f"https://{ndfc.ip4}/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics"
-ndfc.get(url_issu)
+
+url_fabrics = f"{url_base}"
+url_fabrics += "/appcenter/cisco/ndfc/api/v1/lan-fabric/rest/control/fabrics"
+
+url_image_platform = f"{url_base}"
+url_image_platform += "/appcenter/cisco/ndfc/api/v1/imagemanagement/rest/policymgnt/platforms"
+
+# Use of of the above example URLs
+ndfc.get(url_fabrics)
 print(f"{json.dumps(ndfc.response_json, indent=4)}")
--- examples/ndfc_raw_post_request.py
+++ examples/ndfc_raw_post_request.py
@@ -4,27 +4,41 @@ Example of a raw GET request to NDFC
 """
 import json
 
-from ndfc_python.log import log
+from ndfc_python.log_v2 import Log
 from ndfc_python.ndfc import NDFC
 from ndfc_python.ndfc_credentials import NdfcCredentials
 
-payload = {
-    "devices": [{"serialNumber": "FDO21120U5D", "policyName": "KR5M"}],
-    "issu": True,
-    "epld": False,
-    "packageInstall": False,
-}
+try:
+    log = Log()
+    log.commit()
+except ValueError as error:
+    MSG = "Error while instantiating Log(). "
+    MSG += f"Error detail: {error}"
+    print(MSG)
 
-logger = log("ndfc_login_log", "INFO", "DEBUG")
 nc = NdfcCredentials()
 ndfc = NDFC()
 ndfc.domain = nc.nd_domain
 ndfc.ip4 = nc.ndfc_ip
-ndfc.logger = logger
 ndfc.password = nc.password
 ndfc.username = nc.username
 ndfc.login()
-url = f"https://{ndfc.ip4}"
-url += "/appcenter/cisco/ndfc/api/v1/imagemanagement/rest/imageupgrade/install-options"
+url_base = f"https://{ndfc.ip4}"
+
+url = f"{url_base}"
+url += f"/appcenter/cisco/ndfc/api/v1/imagemanagement/rest/policymgnt/platform-policy"
+
+payload = {
+    "policyName": "MyPolicy",
+    "policyType": "PLATFORM",
+    "nxosVersion": "10.3.1_nxos64-cs_64bit",
+    "packageName": "",
+    "platform": "N9K",
+    "policyDescr": "Policy notes",
+    "epldImgName": "",
+    "rpmimages": "",
+    "agnostic": True
+}
+
 ndfc.post(url, payload=payload)
 print(f"{json.dumps(ndfc.response_json, indent=4)}")
