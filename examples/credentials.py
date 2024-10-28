38d88ee5f1e332d7a9b004b464322c0bfbb5dc68
Allen Robel <arobel@cisco.com>
2024-10-28 08:27:53 -1000

    Reorganize credentials (#56)
    * Reorganize credentials
    
    Move credentials libraries into lib/ndfc_python/credentials/*
    
    This is in anticipation of loading credentials from multiple sources, including ENV and CLI.
    
    Rename AnsibleVaultCredentials to CredentialsAnsibleVault in anticipation of adding CredentialsCli and CredentialsEnv.
    
    * Appease linters and update documentation
    
    Fix link in overview.md
    
    * Rename instance var to match new class name

--- examples/credentials.py
+++ examples/credentials.py
@@ -9,7 +9,7 @@ import argparse
 import logging
 import sys
 
-from ndfc_python.ansible_vault_credentials import AnsibleVaultCredentials
+from ndfc_python.credentials.credentials_ansible_vault import CredentialsAnsibleVault
 from ndfc_python.ndfc_python_logger import NdfcPythonLogger
 from ndfc_python.parsers.parser_ansible_vault import parser_ansible_vault
 from ndfc_python.parsers.parser_loglevel import parser_loglevel
@@ -43,18 +43,19 @@ log = logging.getLogger("ndfc_python.main")
 log.setLevel = args.loglevel
 
 try:
-    avc = AnsibleVaultCredentials()
-    avc.ansible_vault = args.ansible_vault
-    avc.commit()
+    cav = CredentialsAnsibleVault()
+    cav.ansible_vault = args.ansible_vault
+    cav.commit()
 except ValueError as error:
-    msg = f"{error}"
+    msg = "Perhaps an incorrect vault password was entered? "
+    msg += f"Error detail: {error}"
     log.error(msg)
     print(msg)
     sys.exit(1)
 
-print(f"nd_domain {avc.nd_domain}")
-print(f"nd_ip4 {avc.nd_ip4}")
-print(f"nd_password {avc.nd_password}")
-print(f"nd_username {avc.nd_username}")
-print(f"nxos_password {avc.nxos_password}")
-print(f"nxos_username {avc.nxos_username}")
+print(f"nd_domain {cav.nd_domain}")
+print(f"nd_ip4 {cav.nd_ip4}")
+print(f"nd_password {cav.nd_password}")
+print(f"nd_username {cav.nd_username}")
+print(f"nxos_password {cav.nxos_password}")
+print(f"nxos_username {cav.nxos_username}")
