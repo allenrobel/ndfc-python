9841228128d1e0f1e57782aa15a1aa3d3dee185a
Allen Robel <arobel@cisco.com>
2024-11-08 11:11:28 -1000

    Updates to doc generation
    util/make_markdown.py
       - Add usage to docstring
    
    lib/ndfc_python/util/markdown_from_template.py
        - initialize self._property_map_file to avoid errors later

--- util/make_markdown.py
+++ util/make_markdown.py
@@ -4,11 +4,27 @@ Name: util/make_markdown.py
 Summary: Generate markdown from an NDFC template
 
 Used to help generate the markdown files in docs/*
+
+cp $HOME/repos/ansible_dev/ndfc_doc_builder/docs/isn.yaml /tmp
+cp $HOME/repos/ansible_dev/ndfc_doc_builder/docs/lan-classic.yaml /tmp
+cp $HOME/repos/ansible_dev/ndfc_doc_builder/docs/msd.yaml /tmp
+cp $HOME/repos/ansible_dev/ndfc_doc_builder/docs/vxlan-evpn.yaml /tmp
+./util/make_markdown.py
+cp /tmp/isn.md $HOME/repos/dcnm-docpoc/docs/modules/dcnm_fabric/isn.md
+cp /tmp/lan-classic.md $HOME/repos/dcnm-docpoc/docs/modules/dcnm_fabric/lan-classic.md
+cp /tmp/msd.md $HOME/repos/dcnm-docpoc/docs/modules/dcnm_fabric/msd.md
+cp /tmp/vxlan-evpn.md $HOME/repos/dcnm-docpoc/docs/modules/dcnm_fabric/vxlan-evpn.md
+
 """
 from ndfc_python.util.markdown_from_template import MarkdownFromTemplate
 
-instance = MarkdownFromTemplate()
-instance.template_file = "/tmp/default_network_universal.yaml"
-instance.markdown_file = "/tmp/default_network_universal.md"
-instance.property_map_file = "./default_network_universal_property_map.yaml"
-instance.commit()
+for file in ["isn.yaml", "lan-classic.yaml", "msd.yaml", "vxlan-evpn.yaml"]:
+    instance = MarkdownFromTemplate()
+    instance.template_file = f"/tmp/{file}"
+    instance.markdown_file = f"/tmp/{file.replace('.yaml', '.md')}"
+    instance.commit()
+# instance = MarkdownFromTemplate()
+# instance.template_file = "/tmp/vxlan-evpn.yaml"
+# instance.markdown_file = "/tmp/vxlan-evpn.md"
+# # instance.property_map_file = "./default_network_universal_property_map.yaml"
+# instance.commit()
