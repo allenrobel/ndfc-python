ad00dce75bbe43923528dad1829d4dc29cca2167
Allen Robel <arobel@cisco.com>
2025-08-21 13:19:19 -1000

    Validator for network attach config

---
+++ lib/ndfc_python/validators/network_attach.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field, PositiveInt, StrictBool
+
+
+class NetworkAttachConfig(BaseModel):
+    """
+    # Summary
+
+    Base validator for NetworkAttach arguments
+    """
+
+    deployment: StrictBool = Field(default=True)
+    detachSwitchPorts: list[str] = Field(alias="detach_switch_ports", default=[])
+    dot1QVlan: PositiveInt | str = Field(alias="dot1q_vlan", default="")
+    extensionValues: str = Field(alias="extension_values", default="")
+    fabric: str = Field(..., alias="fabric_name")
+    freeformConfig: list[str] = Field(alias="freeform_config", default=[])
+    instanceValues: str = Field(alias="instance_values", default="")
+    msoCreated: StrictBool = Field(alias="mso_created", default=False)
+    msoSetVlan: StrictBool = Field(alias="mso_set_vlan", default=False)
+    networkName: str = Field(..., alias="network_name")
+    serialNumber: str = Field(..., alias="serial_number")
+    switchPorts: list[str] = Field(alias="switch_ports", default=[])
+    torPorts: list[str] = Field(alias="tor_ports", default=[])
+    untagged: StrictBool = Field(default=True)
+    vlan: PositiveInt | str = Field(default="")
+
+
+class NetworkAttachConfigValidator(BaseModel):
+    """
+    # Summary
+
+    config is a list of NetworkAttachConfig
+    """
+
+    config: list[NetworkAttachConfig]
