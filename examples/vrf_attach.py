2be954a681e2fb9046c6ce926d6fca96633c3e7e
Allen Robel <arobel@cisco.com>
2025-08-21 20:51:05 -1000

    vrf_attach.py script, library, configs, documentation
    1. New vrf_attach.py script to attach VRFs to switches.
    2. Example configuration for this script
    3. Documentation for this script.
    4. Align vrf_create.py example configuration with vrf_attach.py configuration

---
+++ examples/vrf_attach.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+"""
+# vrf_attach.py
+
+## Description
+
+Attach a VRF
+
+## Usage
+
+1.  Modify PYTHONPATH appropriately for your setup before running this script
+
+``` bash
+export PYTHONPATH=$PYTHONPATH:$HOME/repos/ndfc-python/lib:$HOME/repos/ansible/collections/ansible_collections/cisco/dcnm
+```
+
+2. Optional, to enable logging.
+
+``` bash
+export NDFC_LOGGING_CONFIG=$HOME/repos/ndfc-python/lib/ndfc_python/logging_config.json
+```
+
+3. Edit ./examples/config/vrf_attach.yaml with desired VRF values
+
+4. Set credentials via script command line, environment variables, or Ansible Vault
+
+5. Run the script (below we're using command line for credentials)
+
+``` bash
+./examples/vrf_attach.py \
+    --config ./examples/config/vrf_attach.yaml \
+    --nd-domain local \
+    --nd-ip4 10.1.1.1 \
+    --nd-password password \
+    --nd-username admin
+
+```
+
+"""
+# pylint: disable=duplicate-code
+import argparse
+import json
+import logging
+import sys
+
+from ndfc_python.ndfc_python_logger import NdfcPythonLogger
+from ndfc_python.ndfc_python_sender import NdfcPythonSender
+from ndfc_python.vrf_attach import VrfAttach
+from ndfc_python.parsers.parser_ansible_vault import parser_ansible_vault
+from ndfc_python.parsers.parser_config import parser_config
+from ndfc_python.parsers.parser_loglevel import parser_loglevel
+from ndfc_python.parsers.parser_nd_domain import parser_nd_domain
+from ndfc_python.parsers.parser_nd_ip4 import parser_nd_ip4
+from ndfc_python.parsers.parser_nd_password import parser_nd_password
+from ndfc_python.parsers.parser_nd_username import parser_nd_username
+from ndfc_python.read_config import ReadConfig
+from ndfc_python.validators.vrf_attach import VrfAttachConfigValidator
+from plugins.module_utils.common.response_handler import ResponseHandler
+from plugins.module_utils.common.rest_send_v2 import RestSend
+from plugins.module_utils.common.results import Results
+from pydantic import ValidationError
+
+
+def vrf_attach(cfg: dict) -> None:
+    """
+    Given a vrf-attach configuration, attach the VRF.
+    """
+    try:
+        instance = VrfAttach()
+        instance.rest_send = rest_send
+        instance.results = Results()
+        instance.deployment = cfg.get("deployment")
+        instance.extension_values = cfg.get("extensionValues", "")
+        instance.fabric_name = cfg.get("fabric", "")
+        instance.freeform_config = cfg.get("freeformConfig", [])
+        instance.instance_values = cfg.get("instanceValues")
+        instance.mso_created = cfg.get("msoCreated", False)
+        instance.mso_set_vlan = cfg.get("msoSetVlan", False)
+        instance.serial_number = cfg.get("serialNumber")
+        instance.switch_ports = cfg.get("switchPorts", [])
+        instance.vlan = cfg.get("vlan")
+        instance.vrf_name = cfg.get("vrfName")
+        instance.commit()
+    except ValueError as error:
+        errmsg = "Error attaching VRF. "
+        errmsg += f"Error detail: {error}"
+        log.error(errmsg)
+        print(errmsg)
+        return
+
+    if instance.rest_send.response_current.get("RETURN_CODE") not in (200, 201):
+        if instance.rest_send.response_current.get("DATA", {}).get("message"):
+            errmsg = instance.rest_send.response_current.get("DATA", {}).get("message")
+        else:
+            errmsg = "Error attaching VRF. "
+            errmsg += f"Controller response: {instance.rest_send.response_current}"
+        log.error(errmsg)
+        print(errmsg)
+        return
+    result_msg = f"VRF {instance.vrf_name} "
+    result_msg += f"attached to fabric {instance.fabric_name}, "
+    result_msg += f"serial number {instance.serial_number}."
+    log.info(result_msg)
+    print(result_msg)
+
+
+def setup_parser() -> argparse.Namespace:
+    """
+    ### Summary
+
+    Setup script-specific parser
+
+    Returns:
+        argparse.Namespace
+    """
+    parser = argparse.ArgumentParser(
+        parents=[
+            parser_ansible_vault,
+            parser_config,
+            parser_loglevel,
+            parser_nd_domain,
+            parser_nd_ip4,
+            parser_nd_password,
+            parser_nd_username,
+        ],
+        description="DESCRIPTION: Attach a VRF.",
+    )
+    return parser.parse_args()
+
+
+args = setup_parser()
+NdfcPythonLogger()
+log = logging.getLogger("ndfc_python.main")
+log.setLevel = args.loglevel
+
+try:
+    user_config = ReadConfig()
+    user_config.filename = args.config
+    user_config.commit()
+except ValueError as error:
+    msg = f"Exiting: Error detail: {error}"
+    log.error(msg)
+    print(msg)
+    sys.exit(1)
+
+try:
+    validator = VrfAttachConfigValidator(**user_config.contents)
+except ValidationError as error:
+    msg = f"{error}"
+    log.error(msg)
+    print(msg)
+    sys.exit(1)
+
+try:
+    ndfc_sender = NdfcPythonSender()
+    ndfc_sender.args = args
+    ndfc_sender.commit()
+except ValueError as error:
+    msg = f"Exiting.  Error detail: {error}"
+    log.error(msg)
+    print(msg)
+    sys.exit(1)
+
+rest_send = RestSend({})
+rest_send.sender = ndfc_sender.sender
+rest_send.response_handler = ResponseHandler()
+rest_send.timeout = 2
+rest_send.send_interval = 5
+
+params_list = json.loads(validator.model_dump_json()).get("config", {})
+
+for params in params_list:
+    vrf_attach(params)
