4fc6164ffcc9284ea8329e62cbc1f22fc618c96d
Allen Robel <arobel@cisco.com>
2025-09-06 10:13:34 -1000

    Add validator for config_deploy configuration
    1. ConfigDeployConfigValidator
    
    Validate the configuration for config_deploy operation.
    
    2. examples/config_deploy.py
    
    Update script to leverage validator.

--- examples/config_deploy.py
+++ examples/config_deploy.py
@@ -51,9 +51,11 @@ from ndfc_python.parsers.parser_nd_ip4 import parser_nd_ip4
 from ndfc_python.parsers.parser_nd_password import parser_nd_password
 from ndfc_python.parsers.parser_nd_username import parser_nd_username
 from ndfc_python.read_config import ReadConfig
+from ndfc_python.validators.config_deploy import ConfigDeployConfigValidator
 from plugins.module_utils.common.response_handler import ResponseHandler
 from plugins.module_utils.common.rest_send_v2 import RestSend
 from plugins.module_utils.common.results import Results
+from pydantic import ValidationError
 
 
 def setup_parser() -> argparse.Namespace:
@@ -81,11 +83,19 @@ log = logging.getLogger("ndfc_python.main")
 log.setLevel(level=args.loglevel)
 
 try:
-    ndfc_config = ReadConfig()
-    ndfc_config.filename = args.config
-    ndfc_config.commit()
+    user_config = ReadConfig()
+    user_config.filename = args.config
+    user_config.commit()
 except ValueError as error:
     msg = f"Exiting: Error detail: {error}"
+    log.error(msg)
+    print(msg)
+    sys.exit(1)
+
+try:
+    validator = ConfigDeployConfigValidator(**user_config.contents)
+except ValidationError as error:
+    msg = f"{error}"
     log.error(msg)
     print(msg)
     sys.exit(1)
@@ -107,17 +117,11 @@ rest_send.response_handler = ResponseHandler()
 # Set the timeout higher to give Nexus Dashboard time to complete the request
 rest_send.timeout = 300  # seconds
 
-# Expecting config file like:
-# config:
-#   - fabric_name: MyFabric1
-#   - fabric_name: MyFabric2
-fabrics = ndfc_config.contents.get("config", [])
+fabrics = user_config.contents.get("config", [])
 
 for fabric in fabrics:
+    # fabric_name is already validated by pydantic
     fabric_name = fabric.get("fabric_name")
-    if not fabric_name:
-        log.error("Missing fabric_name in config entry: %s", fabric)
-        continue
     try:
         instance = ConfigDeploy()
         instance.rest_send = rest_send
---
+++ lib/ndfc_python/validators/config_deploy.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class ConfigDeployConfig(BaseModel):
+    """
+    # Summary
+
+    Base validator for ConfigDeploy arguments
+    """
+
+    fabric_name: str = Field(..., min_length=1, max_length=64, description="Name of the fabric")
+
+
+class ConfigDeployConfigValidator(BaseModel):
+    """
+    # Summary
+
+    config is a list of ConfigDeployConfig
+    """
+
+    config: List[ConfigDeployConfig]
