e2898c6ae06edc7086e742f044c28300ce2bceaa
Allen Robel <arobel@cisco.com>
2025-09-06 11:02:28 -1000

    ConfigSave: Add pydantic validator
    Similar to last commit, add a validator specific to config_save operation.

---
+++ lib/ndfc_python/validators/config_save.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class ConfigSaveConfig(BaseModel):
+    """
+    # Summary
+
+    Base validator for ConfigSave arguments
+    """
+
+    fabric_name: str = Field(..., min_length=1, max_length=64, description="Name of the fabric")
+
+
+class ConfigSaveConfigValidator(BaseModel):
+    """
+    # Summary
+
+    config is a list of ConfigSaveConfig
+    """
+
+    config: List[ConfigSaveConfig]
